8c4fbd64ed43c12cb834745efc2514540ceb68e1
Jake Handrick <jake.handrick@gmail.com>
2020-11-11 22:05:10 -0600

    card changes

--- js/js.js
+++ js/js.js
@@ -10,4 +10,32 @@ console.log("addActive")
 }
 });
 
-//LOADING SKILL BARS
+//NAV DISSAPEAR ON SCROLL
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
+
+//CARD DROP SHADOW
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+console.log("document is ready");
+  $( ".card" ).hover(
+  function() {
+    $(this).addClass('shadow-sm').css('cursor', 'pointer');
+  }, function() {
+    $(this).removeClass('shadow-sm');
+  }
+);
+// document ready
+});
