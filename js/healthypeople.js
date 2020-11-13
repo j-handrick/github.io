9faafd5a6bec75199418a342b45bcafd99c8f509
Jake Handrick <jake.handrick@gmail.com>
2020-11-13 15:36:32 -0600

    More portfolio.

--- js/healthypeople.js
+++ js/healthypeople.js
@@ -1,8 +1,10 @@
-$('img').on('click', function(e) {
-  $('#imgViewer').html('').append( $(e.currentTarget).clone().removeClass('img-responsive').removeClass('img-thumbnail') )
-  $('#viewImg').modal('show')
-})
-
-$('img').each(function(i,e) {
-  $(e).wrap('<div class="img-wrapper"></div>')
-})
+//Image Modal: 1
+$("#pop").on("click", function() {
+  $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+  $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+//Image Modal: 2
+$("#pop2").on("click", function() {
+  $('#imagepreview2').attr('src', $('#imageresource2').attr('src')); // here asign the image to the modal when the user click the enlarge link
+  $('#imagemodal2').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
--- js/js.js
+++ js/js.js
@@ -39,9 +39,3 @@ console.log("document is ready");
 );
 // document ready
 });
-
-//Image Modals: 
-$("#pop").on("click", function() {
-  $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
-  $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-});
