a4c5ee7baeea05a144d5e5e7367a757b7324c298
Jake Handrick <jake.handrick@gmail.com>
2020-12-14 11:27:40 -0600

    interaction design

--- js/js.js
+++ js/js.js
@@ -40,4 +40,31 @@ console.log("document is ready");
 // document ready
 });
 
-//RESUME MODAL
+//MODULE SLIDE-INS 
+(function ($) {
+$.fn.visible = function(partial) {
+    
+  var $t            = $(this),
+      $w            = $(window),
+      viewTop       = $w.scrollTop(),
+      viewBottom    = viewTop + $w.height(),
+      _top          = $t.offset().top,
+      _bottom       = _top + $t.height(),
+      compareTop    = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+};
+})(jQuery);
+
+$(window).scroll(function(event) {
+  
+  $(".module").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
