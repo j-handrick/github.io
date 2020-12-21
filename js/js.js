60fb91dd515c63956c5b6b3568d3f9ed882da3fe
Jake Handrick <jake.handrick@gmail.com>
2020-12-21 10:50:49 -0600

    big changes

--- js/js.js
+++ js/js.js
@@ -68,3 +68,20 @@ $(window).scroll(function(event) {
   
 });
 
+// Background Magic 
+(function() {
+
+  let magic = document.querySelector('.magic');
+  let magicWHalf = magic.offsetWidth / 2;
+
+  document.body.addEventListener('mousemove',function(e){
+    magic.style.left = e.pageX - magicWHalf+'px';
+    magic.style.top = e.pageY - magicWHalf+'px';
+  });
+
+  document.body.addEventListener('mouseout',function(e){
+    //magic.style.left = 'calc(50% - 10rem)';
+    //magic.style.top = 'calc(50% - 10rem)';
+  });
+
+})();
